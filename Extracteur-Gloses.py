9ea1875e19d2250b44a05e08358c3ef6f6e64976
DSK <sekou.diao@gmail.com>
2014-10-16 00:09:03 +0200

    Changed word tag to  'syntagm' And tweaked xml UTF-8 encoding.

--- Extracteur-Gloses.py
+++ Extracteur-Gloses.py
@@ -33,10 +33,11 @@ def nettoyer_liste(liste_mot):
     while '\n' in liste_mot:
         liste_mot.remove('\n')
     for mot in liste_mot:
+        index = liste_mot.index(mot)
         if '\n' in mot:
-            index = liste_mot.index(mot)
             i = mot.find('\n')
-            liste_mot[index] = mot[:i]
+            mot = mot[:i]
+        liste_mot[index] = mot.rstrip()
     return
 
 
@@ -216,7 +217,7 @@ def save_xml(xml_obj, filename):
     :param filename:
     :rtype :
     """
-    with codecs.open('{}.xml'.format(filename), 'w') as xml_file:
+    with codecs.open('{}.xml'.format(filename), 'w', encoding='utf8') as xml_file:
         xml_file.write(xml_obj.prettify())
         xml_file.close()
     return
@@ -234,7 +235,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     file_name = args.file_name
-    with codecs.open(file_name, 'r') as file_obj:
+    with codecs.open(file_name, 'r', encoding='utf8') as file_obj:
         extract_gloses(file_obj)
     # file_obj = args.file
     # extract_gloses(file_obj)
