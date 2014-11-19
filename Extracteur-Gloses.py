6816a92b77907fe2ee73630178f8cb517e910a3e
DSK <sekou.diao@gmail.com>
2014-11-20 00:15:31 +0100

    Added sqlite functionality. Now the glosses can be stored in a database with tables 'words' and 'answers'

--- Extracteur-Gloses.py
+++ Extracteur-Gloses.py
@@ -177,7 +177,7 @@ def update_db(dbname, dic):
     gloses = glosesdb.cursor()
     compteur_mot = {}
     for key in dic:
-        for mot in dic[key].keys():
+        for mot in sorted(dic[key].keys()):
             if "$" in mot:
                 mot_nu = mot.rstrip("$")
             elif "#" in mot:
